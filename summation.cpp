95e67c09616e1a74a7b91a209a64a8cf5255c560
yasuharu-sato <yasuharu_sato@icloud.com>
2022-12-14 14:29:47 +0900

    Fix

--- summation.cpp
+++ summation.cpp
@@ -3,9 +3,18 @@
 #include <math.h>
 
 double summation(double plus, double multiply, int num) {
-    long double result = 1.0;
+    long double result = 0.0;
+
+    // if (plus == 0) {
+    //     for (int i=0; i<num; i++){
+    //         result += pow(multiply, num);
+    //     }
+    // }
+
+    long double x = 1.0;
     for (int i=0; i<num; i++){
-        result = result * multiply + plus;
+        x = x * multiply + plus;
+        result += x;
     }
     return result;
 }
