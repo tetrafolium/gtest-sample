ace05e22a25af74419b0fe62d684b09c43049ca4
yasuharu-sato <yasuharu_sato@icloud.com>
2022-12-14 19:52:31 +0900

    fix

---
+++ euclidean-algo.cpp
@@ -0,0 +1,16 @@
+int greatest_common_divisor(int a, int b) {
+  int r, tmp;
+  if(a<b){
+    tmp = a;
+    a = b;
+    b = tmp;
+  }
+ 
+  r = a % b;
+  while(r!=0){
+    a = b;
+    b = r;
+    r = a % b;
+  }
+  return b;
+}
