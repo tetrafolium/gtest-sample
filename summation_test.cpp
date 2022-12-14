fa02de6146a99b6b0a5de586c416ad06b10dc558
yasuharu-sato <yasuharu_sato@icloud.com>
2022-12-14 14:00:27 +0900

    Fix

--- summation_test.cpp
+++ summation_test.cpp
@@ -2,17 +2,17 @@
 #include "summation.h"
 
 TEST(summation_test, answer_05_multiply_2_num){
-    EXPECT_EQ(1.75, summation(0, 0.5, 2));
+    EXPECT_EQ(0.75, summation(0, 0.5, 2));
 }
 
 TEST(summation_test, answer_05_multiply_2_num_s){
-    EXPECT_EQ(1.5, summation(0, 0.5, 2));
+    EXPECT_EQ(0.5, summation(0, 0.5, 2));
 }
 
 TEST(summation_test, answer_05_multiply_100){
-    EXPECT_EQ(2.0, summation(0, 0.5, 100));
+    EXPECT_EQ(1.0, summation(0, 0.5, 100));
 }
 
 TEST(summation_test, answer_08_multiply_100){
-    EXPECT_EQ(2.0, summation(0, 0.8, 100));
+    EXPECT_EQ(1.0, summation(0, 0.8, 100));
 }
