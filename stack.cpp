e8bcf4f7ccfa57507335a1dcd63886324a57b1b5
yasuharu-sato <yasuharu_sato@icloud.com>
2022-12-14 17:50:51 +0900

    Fix

--- stack.cpp
+++ stack.cpp
@@ -6,20 +6,20 @@ Stack<E>::Stack(){
 
 template <typename E>
 void Stack<E>::Push(const E& element){
-  queList.push_front(element);
+  stackList.push_front(element);
 }
 
 template <typename E>
 E* Stack<E>::Pop(){
   E* element = NULL;
-  if (queList.size()>0) {
-    element = &queList.front();
-    queList.pop_front();
+  if (stackList.size()>0) {
+    element = &stackList.front();
+    stackList.pop_front();
   }
   return element;
 }
 
 template <typename E>
 size_t Stack<E>::size() const{
-  return queList.size();
+  return stackList.size();
 }
