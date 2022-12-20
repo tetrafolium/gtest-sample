d330af346eee91007c4bbb26b9bd96e1d0908703
yasuharu-sato <yasuharu_sato@icloud.com>
2022-12-20 13:22:19 +0900

    fix

--- euclidean-algo_test.cpp
+++ euclidean-algo_test.cpp
@@ -1,12 +1,12 @@
 #include <gtest/gtest.h>
 #include "euclidean-algo.cpp"
 
-TEST(euclidean-algo, test1){
+TEST(euclidean_algo, test1){
     EXPECT_EQ(1, greatest_common_divisor(37, 41));
     EXPECT_EQ(1, greatest_common_divisor(37, 51));
     EXPECT_EQ(1, greatest_common_divisor(38, 52));
 }
 
-TEST(euclidean-algo, test1){
+TEST(euclidean_algo, test1){
     EXPECT_EQ(8, greatest_common_divisor(8, 16));
 }
